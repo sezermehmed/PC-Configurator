b36b17d94ced458b00f6f8374d4f1b2d8e2db118
Sezer <sezermehmed98@gmail.com>
2023-05-29 18:32:16 +0300

    .

--- src/main/java/com/restful/restful/WebConfig.java
+++ src/main/java/com/restful/restful/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000","http://localhost:3001","http://localhost:8080")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
--- src/main/java/com/restful/restful/repository/MemoryRepository.java
+++ src/main/java/com/restful/restful/repository/MemoryRepository.java
@@ -2,13 +2,14 @@ package com.restful.restful.repository;
 
 
 import com.restful.restful.model.Memory;
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MemoryRepository extends JpaRepository<Memory, Long> {
-
-
 }
 
 
